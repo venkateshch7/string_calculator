4d855baa199ed2dcac866a79438892028e0c87cd
venkateshwarlu <chvenkatesh255@gmail.com>
2024-05-14 02:39:23 +0530

    T2: when the input string contains only one value

--- spec/sting_calculator_spec.rb
+++ spec/sting_calculator_spec.rb
@@ -9,4 +9,13 @@ RSpec.describe StringCalculator do
     end
 
   end
+
+  context "when the input string contains single value" do
+    subject(:output) { described_class.calculate("3") }
+
+    it "should return 3" do
+      expect(output).to eq(3)
+    end
+
+  end
 end
