cfd8aa64cafbba060ba21c273848972a83eb0d52
venkateshwarlu <chvenkatesh255@gmail.com>
2024-05-14 03:59:50 +0530

    T3: added condition in calculate method to handle 2 numbers

--- string_calculator/string_calculator.rb
+++ string_calculator/string_calculator.rb
@@ -1,6 +1,10 @@
 class StringCalculator
     def self.calculate(value)
         return 0 if value.empty?
-        return value.to_i
+        if value.include? ","
+            return value.split(",").map(&:to_i).sum
+        else
+            return value.to_i
+        end
     end 
 end
