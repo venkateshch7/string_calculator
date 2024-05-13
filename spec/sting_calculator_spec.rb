941e508c4a044646ad6864348da19a347d3ab5c5
venkateshwarlu <chvenkatesh255@gmail.com>
2024-05-14 04:26:52 +0530

    T3: refactoring test cases

--- spec/sting_calculator_spec.rb
+++ spec/sting_calculator_spec.rb
@@ -1,36 +1,47 @@
 require './string_calculator/string_calculator'
 
 RSpec.describe StringCalculator do
-  context "when the input string is empty" do
-    subject(:output) { described_class.calculate("") }
-    
-    it "should return 0" do
+  subject(:output) { described_class.calculate(input) }
+  let(:input) { '' }
+
+  context 'when the input string is empty' do
+    it 'should return 0' do
       expect(output).to eq(0)
     end
-
   end
 
-  context "when the input string contains single value" do
-    subject(:output) { described_class.calculate("3") }
+  context 'when the input string contains single value' do
+    let(:input) { '3' }
 
-    it "should return 3" do
+    it 'should return 3' do
       expect(output).to eq(3)
     end
   end
 
-  context "when the input string contains 2 numbers" do
-    subject(:output) { described_class.calculate("1,2") }
+  context 'when the input string contains 2 numbers' do
+    let(:input) { '1,2' }
 
-    it "should return 3" do
+    it 'should return 3' do
       expect(output).to eq(3)
     end
   end
 
-  context "when the input string contains multiple numbers" do
-    subject(:output) { described_class.calculate("1,2,3") }
-    
-    it "should return 5 when input is '1,2,3'" do
-      expect(output).to eq(6)
+  describe 'when the input string contains multiple numbers' do
+
+    context 'when the input in is 1,2,3' do
+      let(:input) { '1,2,3' }    
+      
+      it 'should return 5' do
+        expect(output).to eq(6)
+      end
+    end
+
+    context 'when the input in is 2,3,4,5' do
+      let(:input) { '2,3,4,5' }
+      
+      it 'should return 14' do
+        expect(output).to eq(14)
+      end
     end
 
   end
