e6232c7fc12566ad78d363dfd6f0e08d2e61a241
venkateshwarlu <chvenkatesh255@gmail.com>
2024-05-14 04:50:39 +0530

    T3: refactoring  code in string_calculator

--- string_calculator/string_calculator.rb
+++ string_calculator/string_calculator.rb
@@ -1,10 +1,6 @@
 class StringCalculator
     def self.calculate(value)
         return 0 if value.empty?
-        if value.include? ","
-            return value.split(",").map(&:to_i).sum
-        else
-            return value.to_i
-        end
+        return value.split(",").map(&:to_i).sum
     end 
 end
