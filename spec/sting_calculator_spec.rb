9f3f47d06ef5842bb9bbffc9278f940db9b6b22e
venkateshwarlu <chvenkatesh255@gmail.com>
2024-05-14 02:22:08 +0530

    T1: when the string is empty

--- spec/sting_calculator_spec.rb
+++ spec/sting_calculator_spec.rb
@@ -1,3 +1,10 @@
 RSpec.describe StringCalculator do
+  context "when the input string is empty" do
+    subject(:output) { described_class.calculate("") }
+    
+    it "should return 0" do
+      expect(output).to eq("0")
+    end
 
+  end
 end
