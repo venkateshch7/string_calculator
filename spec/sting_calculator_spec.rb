f96fb30b2000f107905dee7d2e78923696aae0ab
venkateshwarlu <chvenkatesh255@gmail.com>
2024-05-14 02:24:39 +0530

    T1: added class and method calculate

--- spec/sting_calculator_spec.rb
+++ spec/sting_calculator_spec.rb
@@ -1,9 +1,11 @@
+require './string_calculator/string_calculator'
+
 RSpec.describe StringCalculator do
   context "when the input string is empty" do
     subject(:output) { described_class.calculate("") }
     
     it "should return 0" do
-      expect(output).to eq("0")
+      expect(output).to eq(0)
     end
 
   end
---
+++ string_calculator/string_calculator.rb
@@ -0,0 +1,5 @@
+class StringCalculator
+    def self.calculate(value)
+        return 0 if value.empty?
+    end 
+end
