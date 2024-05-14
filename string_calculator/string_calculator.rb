96fae429c648fdfd6135931217fe651a95d037b8
venkateshwarlu <chvenkatesh255@gmail.com>
2024-05-14 18:42:26 +0530

    T6: added code in StringCalculator to check for negative numbers

--- string_calculator/string_calculator.rb
+++ string_calculator/string_calculator.rb
@@ -2,6 +2,7 @@ class StringCalculator
     def self.calculate(value)
         return 0 if value.empty?
         delimiter, value = self.check_for_delimiter(value)
+        self.check_for_negative_numbers(delimiter,value)
         return value.split(delimiter).map(&:to_i).sum
     end
     
@@ -12,5 +13,11 @@ class StringCalculator
             value = value.split(delimiter+'\n').last
         end
         [delimiter, value]
+    end
+
+    def self.check_for_negative_numbers(delimiter,value)
+        values = value.split(delimiter).map(&:to_i)
+        negative_values = values.map(&:to_i).select{|a| a < 0}
+        raise "negative numbers not allowed: #{negative_values.join(',')}" if negative_values.length > 0
     end
 end
