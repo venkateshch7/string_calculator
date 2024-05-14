7c8b166875d596bdb653a4bbb8a799d891918777
venkateshwarlu <chvenkatesh255@gmail.com>
2024-05-14 17:41:57 +0530

    T5: refactored code in StringCalculator class to add check for delimiter

--- string_calculator/string_calculator.rb
+++ string_calculator/string_calculator.rb
@@ -1,11 +1,16 @@
 class StringCalculator
     def self.calculate(value)
         return 0 if value.empty?
+        delimiter, value = self.check_for_delimiter(value)
+        return value.split(delimiter).map(&:to_i).sum
+    end
+    
+    def self.check_for_delimiter(value)
+        delimiter = /\\n|,/
         if value[0..1] == "//"
             delimiter = value[2...value.length-1].split('\n').first
-            return value.split(delimiter+'\n').last.split(delimiter).map(&:to_i).sum
-        else
-            return value.split(/\\n|,/).map(&:to_i).sum
+            value = value.split(delimiter+'\n').last
         end
-    end 
+        [delimiter, value]
+    end
 end
