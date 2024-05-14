106b551e78b2a2af30dddeb3946e376aa9ba6e80
venkateshwarlu <chvenkatesh255@gmail.com>
2024-05-14 17:31:08 +0530

    T5: added code in calculate method to check for custom delimiter

--- string_calculator/string_calculator.rb
+++ string_calculator/string_calculator.rb
@@ -1,6 +1,11 @@
 class StringCalculator
     def self.calculate(value)
         return 0 if value.empty?
-        return value.split(/\\n|,/).map(&:to_i).sum
+        if value[0..1] == "//"
+            delimiter = value[2...value.length-1].split('\n').first
+            return value.split(delimiter+'\n').last.split(delimiter).map(&:to_i).sum
+        else
+            return value.split(/\\n|,/).map(&:to_i).sum
+        end
     end 
 end
